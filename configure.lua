0da669589afd188914fa2c9fcc7c772b490de3b3
Kamanashis Roy <kamanashisroy+aroopcode@gmail.com>
2014-03-27 17:41:16 +0600

    We have added ellegant configure script

--- configure.lua
+++ configure.lua
@@ -1,24 +1,67 @@
+function yes_no_to_bool(x)
+	if(x == "y") then return true end
+	return false
+end
 
-function promt()
-	local x = 'no'
+function prompt_yes_no(y)
+	local x = "n"
+	repeat
+		 io.write(y)
+		 io.flush()
+		 x=io.read()
+	until x=="y" or x=="n"
 	return x
 end
 
-local conf = assert(io.open("./.config.mk", "w"));
-conf:write("PLATFORM=".."linux\n")
+function prompt(y,xval)
+	local x = xval
+	io.write(y)
+	io.flush()
+	x=io.read()
+	if x == "" then
+		return xval
+	end
+	return x
+end
+
+local configLines = {}
+local configOps = {}
+
+io.write("This is the configure script built for shotodol\n")
+configLines["PLATFORM"] = "linux"
+
 -- LUA_INCLUDE=-I/usr/include/lua5.1
 -- LUA_LIB=-L/usr/lib/ -llua5.1
+-- print("QT_HOME="..."/home/ayaskanti/opt/qt/Desktop/Qt/474/gcc")
 -- print("ECHO="..."echo -e")
 -- use only echo in mac
-conf:write("ECHO=".."echo\n")
--- print("QT_HOME="..."/home/ayaskanti/opt/qt/Desktop/Qt/474/gcc")
-conf:write("SHOTODOL_HOME=/root/projects/shotodol\n")
-conf:write("PROJECTPATH=/root/projects/shotodol\n")
-conf:write("PROJECT_HOME=/root/projects/shotodol\n")
-conf:write("VALA_HOME=/root/projects/aroop/aroop\n")
-conf:write("CFLAGS+=-ggdb3\n")
-conf:write("LINUX_BLUETOOTH=yes\n")
-conf:write("CFLAGS+=-DLINUX_BLUETOOTH\n")
-conf:write("CFLAGS+=-DPLUGIN_ROOT=\"$(SHOTODOL_HOME)\"\n")
+configLines["ECHO"] = "echo"
+local haslfs,lfs = pcall(require,"lfs")
+local phome = "";
+if haslfs then
+	phome = lfs.currentdir()
+end
+configLines["PROJECT_HOME"] = prompt("Project path " .. phome .. " > " , phome)
+configLines["SHOTODOL_HOME"] = configLines["PROJECT_HOME"]
+local ahome = string.gsub(configLines["SHOTODOL_HOME"],"shotodol$","aroop")
+configLines["VALA_HOME"] = prompt("Aroop path " .. ahome .. " > ", ahome)
+configLines["LINUX_BLUETOOTH"] = prompt_yes_no("enable bluetooth ?(y/n) > ")
+configLines["CFLAGS+"] = ""
+if yes_no_to_bool(prompt_yes_no("enable debug (ggdb3) ?(y/n) > ")) then
+	configLines["CFLAGS+"] = configLines["CFLAGS+"] .. " -ggdb3"
+end
+if yes_no_to_bool(configLines["LINUX_BLUETOOTH"]) then
+	configLines["CFLAGS+"] = configLines["CFLAGS+"] .. " -DLINUX_BLUETOOTH"
+end
+configLines["CFLAGS+"] = configLines["CFLAGS+"] .. " -DPLUGIN_ROOT=\\\"$(PROJECT_HOME)/\\\""
+
+local conf = assert(io.open("./.config.mk", "w"))
+for x in pairs(configLines) do
+	local op = configOps[x]
+	if op == nil then
+		op = "="
+	end
+	conf:write(x .. op .. configLines[x] .. "\n")
+end
 assert(conf:close())
 
--- linux/platform_net/staticlibs.mk
+++ linux/platform_net/staticlibs.mk
@@ -2,7 +2,7 @@
 PLATFORM_NET_CSOURCES=$(wildcard $(SHOTODOL_HOME)/$(PLATFORM)/platform_net/csrc/*.c)
 PLATFORM_NET_VSOURCE_BASE=$(basename $(notdir $(PLATFORM_NET_CSOURCES)))
 OBJECTS+=$(addprefix $(OBJDIR)/, $(addsuffix .o,$(PLATFORM_NET_VSOURCE_BASE)))
-ifeq ($(LINUX_BLUETOOTH), "yes") 
+ifeq ($(LINUX_BLUETOOTH), "y") 
 LIBS+=-lbluetooth
 endif
 
